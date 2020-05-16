09c1045b6e5669790d2b312c6b83334fabb7801e
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-05-16 16:50:36 +0200

    Erster Testcommit mit Submodule TLEInterpreter

---
+++ Uebung02_SGP4Propagator/SGP4Propagator.h
@@ -0,0 +1,68 @@
+#ifndef _sgp4_sgp4Propagator_
+#define _sgp4_sgp4Propagator_
+
+#include "../tle/tle.h"
+
+// -------------------------- structure declarations ----------------------------
+
+/**
+ * @brief Simple 3D Cartesian coordinate in ECI
+ */
+struct ECICoordinate
+{
+	float x = 0, y = 0, z = 0;
+
+	/**
+	 * @brief prints the coordinate
+	 */
+	void print();
+};
+
+/**
+ * @brief This class implements the SGP4 orbit propagator
+ *
+ * @author Andreas Maurer, andreas.maurer@uni-wuerzburg.de
+ */
+class SGP4Propagator
+{
+private:
+	Tle tle; //< contains the TLE used for the orbit propagation
+	bool neConstsInitialized = false; ///< indicates if the near earth constants were initialized
+
+public:
+	SGP4Propagator();
+
+	/**
+	 * @brief Sets the TLE used for the orbit propagation with the SGP4 model
+	 *
+	 * This function is also used to update the TLE once a new one is available.
+	 */
+	inline void setTle(const Tle &_tle)
+	{
+		//set the TLE to the new value
+		this->tle = _tle;
+
+		//mark initialization of near earth constant as NOT done
+		neConstsInitialized = false;
+	}
+
+	/**
+	 * @return true if the NearEarthConstant are initialized
+	 */
+	inline bool areNeConstsInitialized() const
+	{
+		return neConstsInitialized;
+	}
+
+	/**
+	 * @brief Calculates the position and velocity of the satellite in the ECI coordinate frame using the SGP4 model
+	 *
+	 * @param secsAfterEpoch	Seconds after the epoch of the TLE [s]
+	 * @param satPos			Position of the satellite in ECI [km]
+	 * @param satVel			Velocity of the satellite in ECI [km/s]
+	 */
+	void calculatePositionAndVelocity(int32_t secsAfterEpoch, ECICoordinate &satPos, ECICoordinate &satVel);
+};
+
+#endif /* end _sgp4_sgp4Propagator_ */
+
