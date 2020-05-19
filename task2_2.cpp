b4bff834dd620d23a18d4879cf99c095ab3bf80f
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-05-19 09:06:59 +0200

    Julian Day Number kann nun zuverlässig ausgerechnet werden. Workaround für Februar und Schalttag eingebaut. Evtl. weiter testen, sonst als fertig ansehen.

--- task2_2.cpp
+++ task2_2.cpp
@@ -54,7 +54,8 @@ int getDayOfMonth(int year, uint16_t days) {
 
     // Aussagenbeschreibung: Jahr ist ein Schaltjahr genau dann wenn...
     // Jahr durch 4 teilbar ist UND ( es NICHT durch 100 teilbar ist ODER es durch 400 teilbar ist)    
-    if ( A & ( !B | C ) ) days_month[1]++; // WICHTIG: kein && oder || verwenden - ALLE drei Ausdrücke MÜSSEN ausgewertet werden!
+    if ( A & ( !B | C ) ) days_month[1]++; // WICHTIG: kein && oder || verwenden - ALLE drei Ausdrücke MÜSSEN 
+    // ausgewertet werden!
 
     // Nachdem Jahrestyp feststeht, beginnen, den Tag des Monats herauszufinden. Dazu so lange
     // durch Monate (Array) iterieren und die ganzen Monate von der Tageszahl abziehen, bis 
@@ -65,18 +66,20 @@ int getDayOfMonth(int year, uint16_t days) {
     }
     // Wird eine Tageszahl größer als 365/366 übergeben, verursacht diese Funktion einen Array Overflow!
     
-    // Schleife bricht ab, wenn Tage kleiner sind als Anzahl Tage des aktuellen Monats, dann sind wir im richtigen Monat gelandet
+    // Schleife bricht ab, wenn aktuelle Anzahl Tage kleiner ist als Anzahl Gesamttage des aktuellen Monats
+    // dann sind wir im richtigen Monat gelandet
     return days;
 }
 
 // Berechnet die Julian Day Number entsprechend des Jahres und des Tagebruchs.
 double computeJD(int year, double dayFractions) {
-    // Ab Stunde die Gleitkommazahlen speichern für Umrechnungen:
-    double hourfrac = exfrac<double>(dayFractions) * 24; 
-    double minutefrac = exfrac<double>(hourfrac) * 60;
-    float secondfrac = exfrac<float>(minutefrac) * 60; // ab hier wirkt sich größere Genauigkeit bei double nur noch marginal aus. Es wird float verwendet um Speicher zu sparen.
-    float milisecondfrac = exfrac<float>(secondfrac) * 100; // 100 ms = 1 s
-    float mikrosecondfrac = exfrac<float>(milisecondfrac) * 100; // 100 mikrosec = 1 ms 
+    // Ab 'Stunde' die Gleitkommazahlen speichern für Umrechnungen:
+    double hour_frac = exfrac<double>(dayFractions) * 24; 
+    double minute_frac = exfrac<double>(hour_frac) * 60;
+    float second_frac = exfrac<float>(minute_frac) * 60; // ab hier (Secondfrac bis Microsecondfrac) wirkt sich 
+    // größere Genauigkeit bei double nur noch marginal aus. Es wird float verwendet um Speicher zu sparen.
+    float milisecond_frac = exfrac<float>(second_frac) * 100; // 100 ms = 1 s
+    float microsecond_frac = exfrac<float>(milisecond_frac) * 100; // 100 mikrosec = 1 ms 
 
     // Alle Variablen in Ganzzahltypen umwandeln:
     // Wichtig: Hier noch nicht mit Julianischen Konstanten rechnen, da es sich noch um "gregorianische" Größen handelt.
@@ -84,42 +87,31 @@ double computeJD(int year, double dayFractions) {
     uint16_t years = year; // Intervall: [0; 2099] negative Jahreszahlen werden nicht berücksichtigt!
     uint8_t months = uabrnd(dayFractions / 30.6001); // Intervall: [0; 11] (Ausnahme von obiger Regel: Liefert hinreichend genaue Angaben des Monats)
     uint16_t days = getDayOfMonth(year, exint(dayFractions)); // Intervall: [0; 365/366]
-    uint8_t hours = exint(hourfrac); // Intervall: [0; 24)
-    uint8_t minutes = exint(minutefrac); // Intervall: [0; 60)
-    uint8_t seconds = exint(secondfrac); // Intervall: [0; 60)
-    uint8_t miliseconds = exint(milisecondfrac); // Intervall: [0; 100)
-    uint8_t mikroseconds = exint(mikrosecondfrac); // Intervall: [0; 100)
-
-    plot(hourfrac)
-    plot(minutefrac)
-    plot(secondfrac)
-    plot(milisecondfrac)
-    plot(mikrosecondfrac)
-    plot("----")
-    plot(years)
-    plot((int)months)
-    plot(days)
-    plot((int)hours)
-    plot((int)minutes)
-    plot((int)seconds)
-    plot((int)miliseconds)
-    plot((int)mikroseconds)
+    uint8_t hours = exint(hour_frac); // Intervall: [0; 24)
+    uint8_t minutes = exint(minute_frac); // Intervall: [0; 60)
+    uint8_t seconds = exint(second_frac); // Intervall: [0; 60)
+    uint8_t miliseconds = exint(milisecond_frac); // Intervall: [0; 100)
+    uint8_t mikroseconds = exint(microsecond_frac); // Intervall: [0; 100)
 
     // ****************************************************************************************
-    // Noch prüfen ob Fallunterscheidung wirklich wegfällt!! (Beispiele ausprobieren!)
-
-    // Fallunterscheidung: month ist größer 0 wird vorausgesetzt (Datentyp unsigned & dayFraction > 0)
-    //if (months <= 2) { years--; months += 12; }
-    //else { ++months; }
+    // Fallunterscheidung: *(month ist größer 0 wird vorausgesetzt (Datentyp unsigned & dayFraction > 0))*
+    // Dient dazu, um Fehler, verursacht durch den Februar (weniger Tage als der Durchschnitt) und durch Schalttage
+    // abzufangen und zu umgehen. Beispiel:
+    // Ohne Fallunterscheidung führt dayFraction 60 (in Schaltjahr 2020 ist das 29. Februar) zu JD von 2458906.5
+    // mit Fallunterscheidung                                                                zu JD von 2458908.5
+    // also zwei Tage Unterschied!
+    if (months <= 2) { years--; months += 12; }
+    // Im Fall (month > 2) können alle Werte ohne Änderungen weiterverwendet werden.
     // ****************************************************************************************
     
-    //++days; // evtl wieder löschen. Ein Tag fehlt nur manchmal... Vielleicht auch erklärbar damit, dass vom 0-ten Tag an gezählt wird?
-
+    // ****************************************************************************************
     ++months; // Monate um 1 inkrementieren, da Julian Calender 0-basierte Monatsindexierung führt
-    // vermutlich fällt deswegen auch hier die Fallunterscheidung weg, da die unter dem Link
-    // angegebene Formel dies nicht erwähnt und ab 1 bis 12 zu zählen beginnt.
-
-
+    // ****************************************************************************************
+    // Quelle: [NOCH EINFÜGEN]
+    // ****************************************************************************************
+    // Berechnung nach der unter QUELLE angegebenen Formel durchführen. Minuten bis Mikrosekunden
+    // wurden händisch hinzugefügt um Genauigkeit zu erhöhen. Nach dem in der Formel angegebenen
+    // Prinzip für Stunden, werden alle weitere Einheiten in Stunden umgerechnet und aufaddiert.
     long double JD =    exint( 365.25 * years ) +
                         exint( 30.6001 * (months + 1) ) +
                         days +
@@ -130,7 +122,8 @@ double computeJD(int year, double dayFractions) {
                         ( mikroseconds / ( 24.0 * 60.0 * 60.0 * 100.0 * 100.0 ) ) +
                         1720981.5
                         ;
-    
+    // ****************************************************************************************
+
     return JD;
     
     // Formel aus der Quelle bezieht Zeit nur bis Stunden ein, Minuten bis Mikrosekunden wurden
