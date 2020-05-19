61e8c14975ffd8a2efa1f88eb4a3e87a177f928d
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-05-20 00:09:05 +0200

    Neue Formel zur Berechnung JD verwendet sowie kleinere Korrekturen der Formel vorgenommen (Nachkommastellen hinzugefügt, Stundenkorrektur von -0.5 zum Endergebnis hinzugefügt um mit gregorianischer Tagesführung übereinzukommen.

--- calendar.cpp
+++ calendar.cpp
@@ -8,6 +8,14 @@
 #define plot(x) std::cout << x << std::endl;
 
 
+// GLOBALE VARIABLEN
+
+const std::array<uint8_t, 12> days_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // Array mit 12 Felder die den Monaten entsprechen und mit
+// Anzahl Tagen gefüllt sind. Wird für Monats- und Tagesberechnung benötigt.
+
+
+// INLINE FUNKTIONEN
+
 // Gibt abgerundete Zahl eines Double (nur unsigned) zurück.
 inline int uabrnd(double decimal) { 
     return static_cast<int>(decimal);
@@ -32,6 +40,9 @@ inline int exint(double decimal) {
     return static_cast<int>(integral);
 }
 
+
+// HILFSFUNKTIONEN
+
 bool checkforLeapYear(uint16_t year) {
     // Schaltjahr-Regel für gregorianischen Kalender:
     // 1. Regel: Jahr ist durch 4 teilbar -> Schaltjahr
@@ -50,24 +61,27 @@ bool checkforLeapYear(uint16_t year) {
 
     // Aussagenbeschreibung: Jahr ist ein Schaltjahr genau dann wenn...
     // Jahr durch 4 teilbar ist UND ( es NICHT durch 100 teilbar ist ODER es durch 400 teilbar ist)    
-    return ( A && ( !B | C ) ); // WICHTIG: kein || verwenden - ALLE beiden Ausdrücke in der "Oder"-Klammer MÜSSEN 
-    // ausgewertet werden, während A zwangsläufig wahr sein muss, damit auch B/C wahr sein können. Hier ist && okay.
-    // Wenn A falsch ist, werden B und C gar nicht erst ausgewertet.
+    return ( A && ( !B | C ) ); // WICHTIG: kein && -oder- || verwenden - ALLE drei Ausdrücke MÜSSEN 
+    // ausgewertet werden um festzustellen ob das Jahr ein Schaltjahr ist, da die Regeln aufeinander aufbauen!
 }
 
-// Hier const definieren, von aufrufenden Funktionen klonen lassen. Vorteil: Nur hier dauerhaft änderbar.
-const std::array<uint8_t, 12> days_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
 // Gibt abhängig von Schaltjahr/Nicht-Schaltjahr und dem Tag des Jahres, den Tag des Monats zurück (z.B. 206 Day of Year = 24. Tag des Monats Juli)
-int getDayOfMonth(uint16_t year, uint16_t days) {
-    //uint8_t days_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; Löschen wenn das Klonen auch funktioniert!
-    
+int getDayOfMonth(uint16_t year, uint16_t days) {   
     // Array klonen, ist dann auch nicht mehr const
     std::array<uint8_t, 12> sub_days_month = days_month;
-    uint8_t& february = sub_days_month[1]; // Zeigt auf zweites Arrayfeld (entspr. Februar)
-    
+
     // Auf Schaltjahr prüfen
-    if ( checkforLeapYear(year) ) february++; // und falls ja, Tage des Februar um 1 erhöhen (29. Februar)
+    bool leapyear = checkforLeapYear(year);
+
+    if( leapyear ) {
+        if (days > 366) return 0; // Bei zu vielen Tagen 0 zurückgeben, da sonst in Schleife Overflow verursacht würde
+
+        sub_days_month[1]++; // Schalttag bei Februar hinzufügen
+    }
+    else
+    {
+        if (days > 365) return 0; // siehe oben
+    }
 
     // Nachdem Jahrestyp feststeht, beginnen, den Tag des Monats herauszufinden. Dazu so lange
     // durch Monate (Array) iterieren und die ganzen Monate von der Tageszahl abziehen, bis 
@@ -76,7 +90,6 @@ int getDayOfMonth(uint16_t year, uint16_t days) {
     {
         days -= sub_days_month[i];
     }
-    // Wird eine Tageszahl größer als 365/366 übergeben, verursacht diese Funktion einen Array Overflow!
     
     // Schleife bricht ab, wenn aktuelle Anzahl Tage kleiner ist als Anzahl Gesamttage des aktuellen Monats
     // dann sind wir im richtigen Monat gelandet
@@ -87,31 +100,30 @@ int getDayOfMonth(uint16_t year, uint16_t days) {
 int getMonthOfYear(uint16_t year, uint16_t days) {
     // Array klonen, ist dann auch nicht mehr const
     std::array<uint8_t, 12> sub_days_month = days_month;
-    uint8_t& february = sub_days_month[1]; // Zeigt auf zweites Arrayfeld (entspr. Februar)
-
+    
     // Auf Schaltjahr prüfen
-    if ( checkforLeapYear(year) ) february++; // und falls ja, Tage des Februar um 1 erhöhen (29. Februar)
+    bool leapyear = checkforLeapYear(year);
 
+    if( leapyear ) {
+        if (days > 366) return 0; // Bei zu vielen Tagen 0 zurückgeben, da sonst in Schleife Overflow verursacht würde
+
+        sub_days_month[1]++; // Schalttag bei Februar hinzufügen
+    }
+    else
+    {
+        if (days > 365) return 0; // siehe oben
+    }
+    
     // Nachdem Jahrestyp feststeht, beginnen, den Monat des Jahres herauszufinden. Dazu so lange
     // durch Monate (Array) iterieren und die ganzen Monate von der Tageszahl abziehen, bis 
     // der nächste Monat bzw. dessen Tage größer ist als die aktuelle Summe => Monat gefunden!
-    uint8_t counter = 0; // Monatszähler
+    uint8_t counter = 1; // Monatszähler. Startet im Januar (0 als Start ergäbe keinen Sinn, wäre außerhalb des Monats)
 
-    while (days > sub_days_month[counter])
+    for (size_t i = 0; days > sub_days_month[i]; i++)
     {
-        days -= sub_days_month[counter];
-        counter++; // Monat inkrementieren
-    } // zum nächsten Monat übergehen
-
-
-    /*
-    PRÜFAUFTRAG: SCHAUEN OB DIE IN DER JD BERECHNUNG IMPLEMENTIERTE INKREMENTIERUNG DES MONATS
-    AUCH HIER VORGENOMMEN WERDEN KÖNNTE. FESTGESTELLT, DASS FÜR DEN AUGUST HIER EINE 7 UND KEINE
-    8 ZURÜCKGEGEBEN WIRD!
-    */
-
-
-    // Verursacht einen Overflow, wenn mehr als 365/366 Tage übergeben werden!
+        days -= sub_days_month[i];
+        counter++;
+    }
     
     // Nach Prinzip getDayOfYear: Wenn Tage kleiner sind als ein voller Monat, sind wir im richtigen Monat gelandet. 
     // Dann einfach Zähler zurückgeben:
@@ -120,18 +132,20 @@ int getMonthOfYear(uint16_t year, uint16_t days) {
 
 // Berechnet die Julian Day Number entsprechend des Jahres und des Tagebruchs.
 double computeJD(int year, double dayFractions) {
+    // ****************************************************************************************
     // Ab 'Stunde' die Gleitkommazahlen speichern für Umrechnungen:
     double hour_frac = exfrac<double>(dayFractions) * 24; 
     double minute_frac = exfrac<double>(hour_frac) * 60;
-    float second_frac = exfrac<float>(minute_frac) * 60; // ab hier (Secondfrac bis Microsecondfrac) wirkt sich 
-    // größere Genauigkeit bei double nur noch marginal aus. Es wird float verwendet um Speicher zu sparen.
+    float second_frac = exfrac<float>(minute_frac) * 60; // ab hier (second_frac bis microsecond_frac) wirkt sich größere Genauigkeit
+    // durch mehr Nachkommastellen in 'double' nur noch marginal aus. Es wird stattdessen 'float' verwendet um Speicher zu sparen.
     float milisecond_frac = exfrac<float>(second_frac) * 100; // 100 ms = 1 s
     float microsecond_frac = exfrac<float>(milisecond_frac) * 100; // 100 mikrosec = 1 ms 
+    // ****************************************************************************************
 
+    // ****************************************************************************************
     // Alle Variablen in Ganzzahltypen umwandeln:
-    // Wichtig: Hier noch nicht mit Julianischen Konstanten rechnen, da es sich noch um "gregorianische" Größen handelt.
-    // Die Umrechnung beginnt erst später in der Berechnung des JDs.
-    uint16_t years = year; // Intervall: [0; 2099] negative Jahreszahlen werden nicht berücksichtigt!
+    //  Wichtig: Hier noch nicht mit Julianischen Konstanten rechnen, da es sich noch um "gregorianische" Größen handelt.
+    //  Die Umrechnung beginnt erst später in der Berechnung des JDs.
     uint8_t months = getMonthOfYear(year, exint(dayFractions)); //uabrnd(dayFractions / 30.6001); // Intervall: [0; 11] (Ausnahme von obiger Regel: Liefert hinreichend genaue Angaben des Monats)
     uint16_t days = getDayOfMonth(year, exint(dayFractions)); // Intervall: [0; 365/366]
     uint8_t hours = exint(hour_frac); // Intervall: [0; 24)
@@ -139,48 +153,57 @@ double computeJD(int year, double dayFractions) {
     uint8_t seconds = exint(second_frac); // Intervall: [0; 60)
     uint8_t miliseconds = exint(milisecond_frac); // Intervall: [0; 100)
     uint8_t microseconds = exint(microsecond_frac); // Intervall: [0; 100)
-
-    // ****************************************************************************************
-    // Fallunterscheidung: *(month ist größer 0 wird vorausgesetzt (Datentyp unsigned & dayFraction > 0))*
-    // Dient dazu, um Fehler, verursacht durch den Februar (weniger Tage als der Durchschnitt) und durch Schalttage
-    // abzufangen und zu umgehen. Beispiel:
-    // Ohne Fallunterscheidung führt dayFraction 60 (in Schaltjahr 2020 ist das 29. Februar) zu JD von 2458906.5
-    // mit Fallunterscheidung                                                                zu JD von 2458908.5
-    // also zwei Tage Unterschied!
-    if (months <= 2) { years--; months += 12; }
-    // Im Fall (month > 2) können alle Werte ohne Änderungen weiterverwendet werden.
-    // ****************************************************************************************
-    
-    plot((int)months)
-    plot(days)
-
-    // ****************************************************************************************
-    ++months; // Monate um 1 inkrementieren, da Julian Calender 0-basierte Monatsindexierung führt
-    // Quelle: [NOCH EINFÜGEN]
-    // ****************************************************************************************
-    // Berechnung nach der unter QUELLE angegebenen Formel durchführen. Minuten bis Mikrosekunden
-    // wurden händisch hinzugefügt um Genauigkeit zu erhöhen. Nach dem in der Formel angegebenen
-    // Prinzip für Stunden, werden alle weitere Einheiten in Stunden umgerechnet und aufaddiert.
-    long double JD =    exint( 365.25 * years ) +
-                        exint( 30.6001 * (months + 1) ) +
-                        days +
-                        ( hours / 24.0 ) +
-                        ( minutes / ( 24.0 * 60.0 ) ) +
-                        ( seconds / ( 24.0 * 60.0 * 60.0 ) ) +
-                        ( miliseconds / ( 24.0 * 60.0 * 60.0 * 100.0 ) ) +
-                        ( microseconds / ( 24.0 * 60.0 * 60.0 * 100.0 * 100.0 ) ) +
-                        1720981.5
-                        ;
     // ****************************************************************************************
 
-    return JD;
-    
+    // ****************************************************************************************
+    // Fallunterscheidung:
+    //  Ist eine Umsortierung der Monate Januar und Februar ans Ende des Jahres um Probleme
+    //  durch die Verkürzung des Februars zu umgehen. Ein eventueller Schalttag ist damit stets
+    //  der letzte Tag des Jahres
+    if (months <= 2) { 
+        year--; months += 12; 
+    }
+    //  Im Fall (month > 2) können alle Werte ohne Änderungen weiterverwendet werden.
+    // ****************************************************************************************
+
+    // ****************************************************************************************
+    // Einzelne Summen berechnen: (Quelle siehe Funktionsende!)
+    uint16_t _A = year - exint( ( 12 - months ) / 10 ); // Intervall bis 65.535
+    uint8_t _M = ( months - 3 ) % 12; // Intervall: [0; 11]
+
+    uint32_t y = exint( 365.25 * ( _A + 4712 ) ); // Intervall bis 25.657.716
+    uint16_t d = exint( ( 30.6001 * _M ) + 0.5 ); // Intervall bis 428 
+
+    uint32_t N = y + d + days + 59; // Intervall: bis 25.658.569
+
+    uint16_t g = exint( exint( ( _A / 100 ) + 49 ) * 0.75 ) - 38; // Intervall bis 490
+    // ****************************************************************************************
+
+    // ****************************************************************************************
+    // Nachkommastellen erstellen:
+    //  Stunden bis Mikrosekunden
+    float frac =    hours / 24.0 +
+                    minutes / ( 24.0 * 60.0 ) +
+                    seconds / ( 24.0 * 60.0 * 60.0 ) +
+                    miliseconds / ( 24.0 * 60.0 * 60.0 * 100.0 ) +
+                    microseconds / ( 24.0 * 60.0 * 60.0 * 100.0 * 100.0 )
+                    ;
+    // ****************************************************************************************
+
+    // ****************************************************************************************
+    // JD aus allen Variablen  zusammensetzen:
+    long double JD = 1.0*N - 1.0*g + frac - 0.5; // - 0.5 folgt aus der unterschiedlichen Tagesdefinition der beiden Kalender:
+    // Gregorianischer Kalender geht von 0-24 Uhr
+    // Julianischer Kalender geht von 12-12
+    // ****************************************************************************************
+
     // Formel aus der Quelle bezieht Zeit nur bis Stunden ein, Minuten bis Mikrosekunden wurden
     // von mir hinzugefügt. Darauf achten: Ab Sekunde ist Umrechnungsfaktor 100, nicht 60!
 
     /*
-    QUELLE: https://gssc.esa.int/navipedia/index.php/Julian_Date
+        QUELLE: Kenneth R. Lang. 1999. Astrophysical Formulae (3. Aufl)., Berlin, Deutschland. S. 72/73 
     */
+    return JD;
 }    
 
 int getUTTime(void) { return 0; } // Noch zu implementieren. Sie unten!
@@ -194,6 +217,12 @@ double computeGMST(double jd) {
     /*
     QUELLE: https://books.google.de/books?id=Nq_1CAAAQBAJ&pg=PA73&lpg=PA73&dq=Julian+Date+Number+in+Greenwich+Meridian+Startime&source=bl&ots=JIHPd5iyiA&sig=ACfU3U3MvCsSi1-C74qO7rnnpcQrxyLnsA&hl=de&sa=X&ved=2ahUKEwjV8KuS_bvpAhUM-qQKHWVkBAMQ6AEwAHoECAoQAQ#v=onepage&q=Julian%20Date%20Number%20in%20Greenwich%20Meridian%20Startime&f=false
     */
+
+
+
+
+
+   return 0;
 }
 
 //GeocentricCoordinate convertECItoGeocentric(const ECICoordinate& eciCoord, double jd);
--- main.cpp
+++ main.cpp
@@ -3,23 +3,56 @@
 
 //#include "TLEinterpreter/process.h"
 //#include "SGP4Propagator/SGP4Propagator.h"
-#include "task2_2.cpp"
+#include "calendar.cpp"
 
 
 
 int main(void) {
     double fraction = 0;
-    int year = 0;
+    int year = 0;//2020;
 
     //fraction = 138.51690000;
 
     std::cin >> year;
     //std::cout << std::endl;
     std::cin >> fraction;
-    std::cout << std::endl;
+    //std::cout << std::endl;
 
     std::cout.precision(15);
     std::cout << computeJD(year, fraction) << std::endl;
+    /*for(int i = 0; i < 365; i++) {
+        long double a = computeJD(year, i + 0.5);
+        long double b = computeJD(year, (i-1) + 0.5);
+        std::cout << computeJD(year, i + 0.5);// << std::endl;
+        if ((a - b) > 1) std::cout << " Unregelmäßigkeit entdeckt " << (a-b);
+        std::cout << std::endl;
+    }*/
+
+    /*for(int i = 2020; i < 2500; i++)
+    {
+        int grenze = 365;
+        if ( checkforLeapYear( i ) ) grenze = 366;
+
+        for (int j = 0; j <= grenze; j++)
+        {
+            long double a = computeJD(i, j + 0.5);
+            long double b = computeJD(i, (j-1) + 0.5);
+
+            if ((a-b) > 1) std::cout << "Fehler in Jahr " << i << " Tag: " << j << std::endl;
+        }
+    }*/
+
+    /*for(int i = 1700; i < 2400; i += 100) {
+        std::cout << i << " ist Schaltjahr: " << (checkforLeapYear(i) ? " WAHR " : " FALSCH ") << std::endl;
+    }*/
+
+    /*for (int i = 1700; i < (2100 + 1); i += 100) {
+        for(int j = 59; j < 61; j++) {
+            std::cout << "Tag " << j << " val: " << computeJD(i, j) << std::endl;
+        }
+
+        std::cout << std::endl;
+    }*/
 
     return 0;
 }
